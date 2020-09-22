e973139edbe88b3f7bcbc3cd3652124345f9b9cc
Philipp Rescheneder <philipp.rescheneder@nanoporetech.com>
2020-09-22 19:17:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # catfishq
-Cat FASTQ files
+Takes paths to an arbritary number of zipped and unzipped FASTQ files and/or folders containing zipped or unzipped FASTQ files, concatenates them and prints them to standard out (default) or an unzipped output file.
+
+# Example
+```bash
+$ catfishq -r test/
+```
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+CatfishQ
+
+"""
+import os
+from setuptools import setup, find_packages
+
+os.environ['GIT_SSL_NO_VERIFY'] = 'true'
+
+__version__ = '1.0.0'
+
+setup(
+    name='catfishq',
+    version=__version__,
+    author='philres',
+    description='Cat FASTQ files',
+    zip_safe=False,
+    install_requires=[
+        'tqdm'
+    ],
+    packages=find_packages(exclude=("tests",)),
+    entry_points={
+        "console_scripts": [
+            'catfishq = catfishq.cat_fastq:main',
+        ]
+    },
+)
